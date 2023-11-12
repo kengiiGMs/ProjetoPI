46fb3fd2d3b302fd4d1fe68c875e8ee7db38d0d9
Miralhas <victor2.miralhas@gmail.com>
2023-11-12 06:35:54 -0300

    displaying sizes left in product page

--- ecommerce/static/ecommerce/script.js
+++ ecommerce/static/ecommerce/script.js
@@ -1,14 +1,58 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const mensagemErro = document.querySelector("#mensagem-erro");
+    const pSpan = document.querySelector("#span_p")
+    const mSpan = document.querySelector("#span_m") 
+    const gSpan = document.querySelector("#span_g") 
+    const ggSpan = document.querySelector("#span_gg") 
 
-    document.querySelector("#review-form").addEventListener("submit", function(event){
-        if (document.querySelector("#starValue").value === ""){
-            event.preventDefault();
-            mensagemErro.style.display = "block";
-        } else {
-            mensagemErro.style.display = "none";
-        }
+    document.querySelectorAll(".tamanho-btn").forEach(btn => {
+        btn.addEventListener("click", function(){
+
+            switch (btn.id){
+                case "tamanho_p":
+                    pSpan.style.display = "block";
+                    mSpan.style.display = "none";
+                    gSpan.style.display = "none";
+                    ggSpan.style.display = "none";
+                    break;
+
+                case "tamanho_m":
+                    pSpan.style.display = "none";
+                    mSpan.style.display = "block";
+                    gSpan.style.display = "none";
+                    ggSpan.style.display = "none";
+                    break;
+                
+                case "tamanho_g":
+                    pSpan.style.display = "none";
+                    mSpan.style.display = "none";
+                    gSpan.style.display = "block";
+                    ggSpan.style.display = "none";
+                    break;
+
+                case "tamanho_gg":
+                    pSpan.style.display = "none";
+                    mSpan.style.display = "none";
+                    gSpan.style.display = "none";
+                    ggSpan.style.display = "block";
+                    break;
+
+            }
+        })
     })
+
+
+    const mensagemErro = document.querySelector("#mensagem-erro");
+    
+    if (document.querySelector("#review-form")){
+        document.querySelector("#review-form").addEventListener("submit", function(event){
+            if (document.querySelector("#starValue").value === ""){
+                event.preventDefault();
+                mensagemErro.style.display = "block";
+            } else {
+                mensagemErro.style.display = "none";
+            }
+        })
+    }
 
     const starWrapper = document.querySelector(".stars");
     const stars = document.querySelectorAll(".stars i");
